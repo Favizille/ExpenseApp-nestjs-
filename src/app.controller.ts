c42b2e594fa9269906306590cf913e7dcdc8997f
Favizille <89010415+Favizille@users.noreply.github.com>
2023-01-11 22:44:34 +0100

    transfered logic from controller to service and used dependency injection in controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,29 +1,42 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { HttpCode } from '@nestjs/common/decorators';
+import { ReportType } from "src/data";
 
+import {AppService} from "./app.service"
 @Controller('report/:type')
 export class AppController {  
+
+  constructor( private readonly appService: AppService){}
+
   @Get()
-  getAllIncomeReports() {
-    return [];
+  getAllIncomeReports( @Param('type') type: string) {
+    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE
+    return this.appService.getAllReports(reportType);
   }
 
   @Get(':id')
-  getIncomeReportsById() {
-    return {};
+  getIncomeReportById( @Param('type') type: string, @Param('id') id: string) {
+    const reportType = type === 'income' ? ReportType.INCOME : ReportType.EXPENSE
+    return this.appService.getReportById(reportType, id);
   }
 
   @Post()
-  createReport(){
-    return "Post";
+  createReport( @Body() {amount, source}: { amount: number; source: string;}, @Param('type') type:string){
+    const reportType = 
+    type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.createReport(reportType, {amount, source});
   }
 
   @Put(':id')
-  updateReport(){
-    return "update income";
+  updateReport( @Param('id') id: string, @Param("type") type : string, @Body() body: {amount: number; source: string}){
+    const reportType = type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return this.appService.updateReport(reportType, id, body);
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  deleteIncome(){
-    return "deleted income";
+  deleteReport(@Param("id") id: string){
+    return this.appService.deleteReport(id);
   }
 }
